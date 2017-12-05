ca74c753f53e105f50b058e770da4a0619cfc131
Aldrich <mascarenhasaldrich@gmail.com>
2017-12-05 14:49:04 +0000

    Update Skylark API Interface

--- app/src/main/java/com/nerdcutlet/skylarktest/data/remote/SkylarkServerInterface.kt
+++ app/src/main/java/com/nerdcutlet/skylarktest/data/remote/SkylarkServerInterface.kt
@@ -3,8 +3,6 @@ package com.nerdcutlet.skylarktest.data.remote;
 import com.nerdcutlet.skylarktest.ui.setdetail.model.remote.ImageResponse;
 import com.nerdcutlet.skylarktest.ui.sets.model.local.SetResponse;
 
-import io.reactivex.Observable;
-import io.reactivex.Single;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
@@ -13,12 +11,12 @@ import retrofit2.http.Path;
  * Created by Aldrich on 14-Nov-17.
  */
 
-public interface SkylarkServerInterface {
+ interface SkylarkServerInterface {
 
     @GET("sets/")
-    Call<SetResponse> getSkylarkSet();
+    fun  getSkylarkSet() : Call<SetResponse>
 
     @GET("images/{image_id}/")
-    Call<ImageResponse> getImage(@Path("image_id") String image_id);
+    fun getImage(@Path("image_id") image_id : String ) : Call<ImageResponse>
 
 }
